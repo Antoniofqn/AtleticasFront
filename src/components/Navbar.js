f9c1f65f3f4540649180ef620114edb86531a9e9
Antonio Neto <antoniofqn@hotmail.com>
2023-12-20 10:55:33 -0300

    update navbar functionalities

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [showUniversidadesDropdown, setShowUniversidadesDropdown] = useState(false);
+  const [showClubsDropdown, setShowClubsDropdown] = useState(false);
+
   return (
     <nav className="bg-orange-700 text-white p-4 flex justify-between items-center">
       <h1 className="text-3xl font-bold">
         <Link to="/">Atléticas App</Link>
       </h1>
       <ul className="flex space-x-4">
-        <li>
-          <Link to="/" className="hover:text-green-100">Universidades</Link>
+        <li className="relative" onMouseEnter={() => setShowUniversidadesDropdown(true)} onMouseLeave={() => setShowUniversidadesDropdown(false)}>
+          <span className="hover:text-green-100 font-bold cursor-pointer">Universidades</span>
+          {showUniversidadesDropdown && (
+            <div className="absolute bg-white text-black w-40 rounded shadow-lg">
+              <Link to="/universities/create" className="block border border-orange-500 px-4 py-2 hover:bg-gray-200 font-bold text-orange-500">Criar Universidade</Link>
+              <Link to="/" className="block border border-orange-500 px-4 py-2 hover:bg-gray-200 font-bold text-orange-500">Listar Universidades</Link>
+            </div>
+          )}
         </li>
-        <li>
-          <Link to="/clubs" className="hover:text-green-100">Atléticas</Link>
+        <li className="relative" onMouseEnter={() => setShowClubsDropdown(true)} onMouseLeave={() => setShowClubsDropdown(false)}>
+          <span className="hover:text-green-100 font-bold cursor-pointer">Atléticas</span>
+          {showClubsDropdown && (
+            <div className="absolute bg-white text-black w-40 rounded shadow-lg right-0">
+              <Link to="/clubs/create" className="block border border-orange-500 px-4 py-2 hover:bg-gray-200 font-bold text-orange-500">Criar Atlética</Link>
+              <Link to="/clubs" className="block border border-orange-500 px-4 py-2 hover:bg-gray-200 font-bold text-orange-500">Listar Atléticas</Link>
+            </div>
+          )}
         </li>
       </ul>
     </nav>
